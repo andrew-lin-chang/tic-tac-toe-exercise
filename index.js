c212cbadfc1fc1a0e8a754dddf7a78d4f5f809ba
Chang <andrewchang@utexas.edu>
2023-06-29 16:08:23 -0500

    Add handling of cell clicks

--- index.js
+++ index.js
@@ -15,41 +15,74 @@ function GameBoard() {
 }
 
 function Cell() {
-    let value = 0;
+    let value = "";
 
-    const addToken = (player) => {
+    const setValue = (player) => {
         value = player;
     };
 
     const getValue = () => value;
 
     return {
-        addToken,
+        setValue,
         getValue
     };
 }
 
 function GameController() {
     const gameBoard = GameBoard();
+    const players = [
+        {
+            name: 'Player 1',
+            token: 'X'
+        },
+        {
+            name: 'Player 2',
+            token: 'O'
+        }
+    ]
+
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
 
     return {
-        getBoard: gameBoard.getBoard(),
+        getActivePlayer,
+        switchPlayer,
+        getBoard: gameBoard.getBoard,
     }
 }
 
 function ScreenController() {
     const game = GameController();
+    const board = game.getBoard();
     const boardDiv = document.querySelector('.game-board');
+
+    const handleClick = (event) => {
+
+        if(event.target.textContent) return; //can't override cells
+
+        const dataset = event.target.dataset;
+        board[dataset.rowIndex][dataset.colIndex].setValue(game.getActivePlayer().token);
+        game.switchPlayer();
+        updateScreen();
+    }
 
     const updateScreen = () => {
         boardDiv.innerHTML = '';
 
-        const board = game.getBoard;
-
-        board.forEach(row => {
-            row.forEach((cell, index) => {
+        board.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
                 const cellBox = document.createElement('div');
                 cellBox.classList.add('cell');
+                cellBox.dataset.rowIndex = rowIndex;
+                cellBox.dataset.colIndex = colIndex;
+                cellBox.textContent = cell.getValue();
+                cellBox.addEventListener('click', handleClick)
                 boardDiv.appendChild(cellBox);
             })
         })
